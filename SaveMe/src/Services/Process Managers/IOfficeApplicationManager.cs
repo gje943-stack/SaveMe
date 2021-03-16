9e1d6d1346d8f8b4b60d0336a4c548bf7ec34d6e
George Everett <gjemba@hotmail.com>
2021-03-16 01:03:39 +0000

    add ApplicationManager (was missed from prev. commit)

---
+++ SaveMe/src/Services/Process Managers/IOfficeApplicationManager.cs
@@ -0,0 +1,12 @@
+﻿using src.Models;
+using System.Collections.Generic;
+
+namespace src.Services.Process_Managers
+{
+    public interface IOfficeApplicationManager
+    {
+        IEnumerable<IOfficeApplication> FetchOpenExcelProcesses();
+        IEnumerable<IOfficeApplication> FetchOpenPowerPointApplications();
+        IEnumerable<IOfficeApplication> FetchOpenWordProcesses();
+    }
+}
